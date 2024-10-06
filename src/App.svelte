1e3030d42732f849b5db1546b554f309d05c548b
underayko <lois.corpuuuz@gmail.com>
2024-10-07 00:07:32 +0800

    Updated

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<!-- src/App.svelte -->
+<script>
+    import Header from './components/Header.svelte';
+    import Portfolio from './components/Portfolio.svelte';
+    import Footer from './components/Footer.svelte';
+</script>
+
+<Header title="My Portfolio" />
+<Portfolio />
+<Footer />
+
